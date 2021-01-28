b87abf6b6d59e023e3bc6506ee3ad195372a34a1
Shawn A Hensley <sahensley@users.noreply.github.com>
2021-01-28 05:53:23 +0000

    start of 12-factor settings conversion

--- dlt_project/settings.py
+++ dlt_project/settings.py
@@ -10,7 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import environ
 from pathlib import Path
+
+env = environ.Env(
+    DEBUG=(bool,False)
+)
+
+# Reads from .env file.  Order: envvar -> .env file -> defaults
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "tuxp33zv(qx=w865m*0!8g*3z&5kj*g$@9i$z+6ihjzund#iud"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
