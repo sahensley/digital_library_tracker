fb0b445466397bb4dc1d21b70218a775defc354b
Shawn A Hensley <sahensley@users.noreply.github.com>
2021-01-31 00:13:49 +0000

    add index urls

--- dlt_project/urls.py
+++ dlt_project/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+
 router = DefaultRouter()
 urlpatterns = [
+    path("", include("core.urls")),
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls")),
