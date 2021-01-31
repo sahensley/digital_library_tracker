9bb19673b80327180d270d88745a7d827b59d299
Shawn A Hensley <sahensley@users.noreply.github.com>
2021-01-30 23:46:41 -0600

    fix line endings from CRLF to LF

--- devsetup.sh
+++ devsetup.sh
@@ -3,9 +3,6 @@
 export DJANGO_SUPERUSER_USERNAME="vscode"
 export DJANGO_SUPERUSER_EMAIL="example@example.com"
 export DJANGO_SUPERUSER_PASSWORD="vscode"
-export DEBUG="True"
-export SECRET_KEY="devsetup_key"
-export ALLOWED_HOSTS='*'
 devdatabase="/workspaces/digital_library_tracker/db.sqlite3"
 
 rm "${devdatabase:?}" 
