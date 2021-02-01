6c46afb95dc26b19b52d67c116ad4153e24cdbe8
Shawn A Hensley <sahensley@users.noreply.github.com>
2021-02-01 00:30:49 -0600

    Add fields to HumbleItem admin list display

--- humblestore/admin.py
+++ humblestore/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import HumbleBundle, HumbleItem
 
+
+class HumbleItemAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "bundle", "is_gift", "claimed_by")
+
+
 admin.site.register(HumbleBundle)
-admin.site.register(HumbleItem)
+admin.site.register(HumbleItem, HumbleItemAdmin)
