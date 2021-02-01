b265581d06c250a89d17ee99c94c583edd279dec
Shawn A Hensley <sahensley@users.noreply.github.com>
2021-02-01 00:30:19 -0600

    Register Platform in core app admin page

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Application, Developer, Person, Publisher
+from .models import Application, Developer, Person, Platform, Publisher
 
 admin.site.register(Application)
 admin.site.register(Developer)
 admin.site.register(Person)
+admin.site.register(Platform)
 admin.site.register(Publisher)
